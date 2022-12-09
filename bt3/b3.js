3440db2c65d27cecc7b117dfdd19352c387c6c7c
HoangTran2108 <119851922+HoangTran2108@users.noreply.github.com>
2022-12-09 23:45:49 +0700

    cc

--- bt3/b3.js
+++ bt3/b3.js
@@ -10,3 +10,15 @@ function r() {
     let q=(+p)-(+h)
     document.getElementById("kq").innerHTML=q
 }
+function n() {
+    let p = document.getElementById("s1").value;
+    let h = document.getElementById("s2").value;
+    let a = (+p) * (+h)
+    document.getElementById("kq").innerHTML = a
+}
+function t() {
+    let p = document.getElementById("s1").value;
+    let h = document.getElementById("s2").value;
+    let b = (+p) / (+h)
+    document.getElementById("kq").innerHTML = b
+}n
